50d05817b318931f36ccd20c61c1150236ab5dab
matte19 <matt.evenden35@gmail.com>
2026-04-28 11:27:22 +0100

    supabase update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,5 +2,16 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  process.env.REACT_APP_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      storageKey: 'netwrkr-auth',
+      storage: window.localStorage,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    }
+  }
 );
+
+window.__supabase = supabase;
