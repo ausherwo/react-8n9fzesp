67e377ae188e24f757a12ed38bcd348580ceb665
ausherwo <au@netwrkr.ai>
2026-06-05 16:45:29 +0100

    post hog

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PostHogProvider } from '@posthog/react';
 
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const posthogOptions = {
+  api_host: process.env.REACT_APP_POSTHOG_HOST,
+  };
 
-root.render(
-    <App />
-);
+  const rootElement = document.getElementById('root');
+  const root = createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+        <PostHogProvider
+              apiKey={process.env.REACT_APP_POSTHOG_KEY}
+                    options={posthogOptions}
+                        >
+                              <App />
+                                  </PostHogProvider>
+                                    </StrictMode>
+                                    );
